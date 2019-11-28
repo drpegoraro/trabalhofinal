5a5e245c459cef85d2da65392a0c6e01b18c2016
drpegoraro <56133734+drpegoraro@users.noreply.github.com>
2019-11-28 19:58:49 -0300

    Create abi.js

---
+++ javascript/abi.js
@@ -0,0 +1,138 @@
+[
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "nomePrestador",
+				"type": "string"
+			},
+			{
+				"internalType": "address payable",
+				"name": "contaPrestador",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "relatorioEmitido",
+		"type": "event"
+	},
+	{
+		"constant": false,
+		"inputs": [],
+		"name": "gerarRelatorio",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "prestador",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "mesTrabalhado",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "anoTrabalhado",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "diaDoMesTrabalhado",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "descricaoTrabalho",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "totalDeHorasTrabalhadas",
+				"type": "uint256"
+			}
+		],
+		"name": "relatarHoras",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "relatos",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "mes",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "ano",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "diaDoMes",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "trabalhoEfetuado",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "totalHorasTrabalhadas",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
