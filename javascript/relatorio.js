2a4d5fa60b459ec1896efc11241409ae5217df89
drpegoraro <56133734+drpegoraro@users.noreply.github.com>
2019-12-02 16:42:34 -0300

    Add files via upload

--- javascript/relatorio.js
+++ javascript/relatorio.js
@@ -1,21 +1,38 @@
-var enderecoContrato = "contract.getContractBalance()";
+var enderecoContrato ="0x557D0Ecd8A9ae210519d1bD9B543723E04941eEe";
 var provedor = new ethers.providers.Web3Provider(web3.currentProvider);
 var signer = provider.getSigner();
 var contract = new ethers.Contract(contractAddress, contractAbi, signer);
 
 function RelatarHoras() 
 {
-    var nomePrestador==document.getElementById("nomePrestador");
+    var nomePrestador=document.getElementById("nomePrestador");
     var mesTrabalhado= document.getElementById("mesTrabalhado");
     var anoTrabalhado= document.getElementById("anoTrabalhado");
     var diaDoMesTrabalhado= document.getElementById("diaDoMesTrabalhado");
     var descricaoTrabalho= document.getElementById("descricaoTrabalho");
     var totalDeHorasTrabalhadas= document.getElementById("totalDeHorasTrabalhadas");
+    .then ( (relatoHoras) =>
+    {
+        console.log("getRelatarHoras, relatoHoras");
+    })
+    .catch ((err) =>
+    {
+        console.error(err);
+        relatoHoras = "Erro ao relatar as horas"
+    })
 }
 
 function gerarRelatorio()
 {
     var apuracaoDoMes=document.getElementById("apuracaoDoMes");
-    contract.getContractBalance()
+    .then ( (apuraMes) =>
+    {
+        console.log("getgerarRelatorio, apuraMes");
+    })
+    .catch ((err) =>
+    {
+        console.error(err);
+        apuraMes = "Erro ao apurar o mês"
+    })
 
 }
